772e4d2e6ecef727fe8ee3bc28c86057dcaa740f
prashantbg05 <77620512+prashantbg05@users.noreply.github.com>
2021-01-29 03:00:33 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -2,37 +2,42 @@
 #include <iostream>
 #include "catch.hpp"
 #include "stats.h"
-
 #include <cmath>
 using namespace std;
 using namespace Statistics;
 
+//Statistics obj;
+//float myarray[4] = {1.5, 8.9, 3.2, 4.5};
 TEST_CASE("reports average, minimum and maximum") {
-        auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
+    auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
     float epsilon = 0.001;
-    REQUIRE(std::abs(computedStats.average - 4.525) < epsilon);
-    REQUIRE(std::abs(computedStats.max - 8.9) < epsilon);
-    REQUIRE(std::abs(computedStats.min - 1.5) < epsilon);
+    REQUIRE(std::abs(computedStats.Average - 4.525) < epsilon);
+    REQUIRE(std::abs(computedStats.Maximum - 8.9) < epsilon);
+    REQUIRE(std::abs(computedStats.Minimum - 1.5) < epsilon);
 }
 
 TEST_CASE("average is NaN for empty array") {
     auto computedStats = Statistics::ComputeStatistics({});
+    REQUIRE(std::isnan(computedStats.Average ) == true);
+    REQUIRE(std::isnan(computedStats.Maximum )  == true);
+    REQUIRE(std::isnan(computedStats.Minimum )  == true);
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
     
     //Design the REQUIRE statement here.
-    //Use http://www.cplusplus.com/reference/cmath/isnan/
+    //Use http://www.cplusplus.com/reference/cmatAverage/
 }
 /*
 TEST_CASE("raises alerts when max is greater than threshold") {
     EmailAlert emailAlert;
     LEDAlert ledAlert;
-    std::vector<IAlerter*> alerters = {&emailAlert, &ledAlert};
-    
+    //std::vector<IAlerter*> alerters = {&emailAlert, &ledAlert};
+    statsAlerter statsAlerterobj;
     const float maxThreshold = 10.2;
-    StatsAlerter statsAlerter(maxThreshold, alerters);
-    statsAlerter.checkAndAlert({99.8, 34.2, 4.5, 6.7});
-
+   // StatsAlerter statsAlerter(maxThreshold, alerters);
+    statsAlerterobj.checkAndAlert({99.8, 34.2, 4.5, 6.7});
+cout<<emailAlert.emailSent<<endl;
+    cout<<ledAlert.ledGlows<<endl;
     REQUIRE(emailAlert.emailSent);
-    REQUIRE(ledAlert.ledGlows);
-}*/
+    REQUIRE(ledAlert.ledGlows);*/
+}
