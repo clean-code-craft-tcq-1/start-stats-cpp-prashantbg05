131230fcd520b6c97e38dc98b944bb7a3125bcfa
prashantbg05 <77620512+prashantbg05@users.noreply.github.com>
2021-01-29 03:49:20 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -2,17 +2,50 @@
 namespace Statistics
 {
     
-class ComputeAVGMaxMin 
+		class CalMeanMaxMin 
+		{
+				public:
+				int size;
+				
+				float Mean;
+				float MaxVal;
+				float MinVal;
+				float CalculateMean(const std::vector<float>& VecItem);
+				float CalculateMax(const std::vector<float>& VecItem);
+				float CalculateMin(const std::vector<float>& VecItem);
+		};
+		
+		
+		CalMeanMaxMin ComputeStatistics(const std::vector<float>& VecItem);
+}
+
+
+class IAlerter
+{
+};
+
+
+class EmailAlert : public IAlerter
 {
     public:
-       int size;
-    //void ComputeStatistics(float report[]);
-    double Average;
-   double Maximum;
-   double Minimum;
-    double CalculateAvg(const std::vector<double>& report);
-    double CalculateMax(const std::vector<double>& report);
-    double CalculateMin(const std::vector<double>& report);
+    bool emailSent;
 };
-   ComputeAVGMaxMin computedStats(const std::vector<double>& report);
-}
+
+class LEDAlert : public IAlerter
+{
+     public:
+      bool ledGlows;
+};
+
+
+class statsAlerter
+{
+    public:
+     EmailAlert emailAlert;
+    LEDAlert ledAlert;
+   float maxThreshold;
+    std::vector<IAlerter*> alerters = {&emailAlert, &ledAlert};
+    //statsAlerter(maxThreshold, alerters);
+    statsAlerter(const float maxThreshold, std::vector<IAlerter*> alerters );
+    void checkAndAlert(const std::vector<float>& alertvalues);
+};
