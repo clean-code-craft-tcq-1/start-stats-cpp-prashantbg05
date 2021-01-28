d2782d8ad5aa4b23d9ebdb1f7884f80d5ec0fb78
prashantbg05 <77620512+prashantbg05@users.noreply.github.com>
2021-01-29 04:02:20 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -5,7 +5,7 @@
 #include <cmath>
 using namespace std;
 using namespace Statistics;
-
+typedef const std::vector<double>&report computedStats;
 //Statistics obj;
 //float myarray[4] = {1.5, 8.9, 3.2, 4.5};
 TEST_CASE("reports average, minimum and maximum") {
