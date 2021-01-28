00468c9e89398ea4996378999f4b34a3e134af13
prashantbg05 <77620512+prashantbg05@users.noreply.github.com>
2021-01-29 03:37:15 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -5,70 +5,15 @@
 using namespace std;
 using namespace Statistics;
 
-    
-ComputeAVGMaxMin Statistics::computedStats(const std::vector<double>& report)
-{
-    //Implement statistics here
-    
-    ComputeAVGMaxMin statreport;
-    statreport.Average = 0;
-    statreport.Maximum = 0;
-    statreport.Minimum = 0;
-    
-    if(!(report.empty()))
-    {
-        statreport.Average = statreport.CalculateAvg(report);
-        statreport.Maximum = statreport.CalculateMax(report);
-        statreport.Minimum = statreport.CalculateMin(report);
-    }
-    else
-    {
-        statreport.Average = NAN;
-        statreport.Maximum = NAN;
-        statreport.Minimum = NAN;
-    }
-  
-    return statreport;
-} 
-double ComputeAVGMaxMin::CalculateAvg(const std::vector<double>& report)
-{
-    double  sum = 0;
-    double  Avg = 0;
-   
-    for(int i=0;i<report.size();i++)
-    {
-        sum = sum + report[i];
-      
-    }
-    Avg = sum/report.size();
 
-    return Avg;
-}
 
-double ComputeAVGMaxMin::CalculateMax(const std::vector<double>& report)
+struct computedStatsstruct 
 {
-    double  Max = report[0];
-    for(int i=1;i<report.size();i++)
-    {
-       if(Max < report[i])
-       {
-          Max =  report[i];
-       }
-    }
-   
-    return Max;
-}
-
-double ComputeAVGMaxMin::CalculateMin(const std::vector<double>& report)
-{
-     double  Min = report[0];
-    for(int i=1;i<report.size();i++)
-    {
-       if(Min > report[i])
-       {
-          Min =  report[i];
-       }
-    }
-   
-    return Min;
-}
+    float average;
+    float max;
+    float min;
+    
+    string straverage;
+    string strmax;
+    string strmin;
+}computedStats;
