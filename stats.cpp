40f1f32543d8934c9f5d558482611176f702a216
prashantbg05 <77620512+prashantbg05@users.noreply.github.com>
2021-01-28 21:19:40 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -23,3 +23,14 @@ Stats Statistics::ComputeStatistics(const std::vector<int>& )
      
       return (mean/4);
     }
+
+namespace App {
+namespace Core {
+
+INSTANTIATE_TEST_CASE_P(
+   stats_Test__ComputeStatistics,
+   stats_Test__ComputeStatistics_WithValidParam,
+   ::testing::Values
+   (
+   
+       )
