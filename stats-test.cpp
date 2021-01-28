c62070da1adafdd3ebb16fbd8e65caae647066e2
prashantbg05 <77620512+prashantbg05@users.noreply.github.com>
2021-01-28 19:49:09 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -4,21 +4,16 @@
 #include "stats.h"
 
 #include <cmath>
-/*
-using Namespace Statistics
-class Statistics
-{
-    
-};
+
 
 TEST_CASE("reports average, minimum and maximum") {
-    auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
+        auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
     float epsilon = 0.001;
     REQUIRE(std::abs(computedStats.average - 4.525) < epsilon);
     REQUIRE(std::abs(computedStats.max - 8.9) < epsilon);
     REQUIRE(std::abs(computedStats.min - 1.5) < epsilon);
 }
-
+/*
 TEST_CASE("average is NaN for empty array") {
     auto computedStats = Statistics::ComputeStatistics({});
     //All fields of computedStats (average, max, min) must be
