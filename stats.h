9bfdd2f58ee57fa7bbf3ea03178ad9602ba7b0a5
prashantbg05 <77620512+prashantbg05@users.noreply.github.com>
2021-01-29 05:28:51 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -2,15 +2,15 @@
 namespace Statistics
 {
     
-class ComputeAVGMaxMin 
+class CalMeanMaxMin  
 {
     public:
        int size;
-    //void ComputeStatistics(float report[]);
+
     double Average;
    double Maximum;
    double Minimum;
-    double CalculateAvg(const std::vector<double>& report);
+    double CalculateMean(const std::vector<double>& report);
     double CalculateMax(const std::vector<double>& report);
     double CalculateMin(const std::vector<double>& report);
 };
