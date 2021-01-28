0234896314f415509541719b13240eb44a2f3d56
prashantbg05 <77620512+prashantbg05@users.noreply.github.com>
2021-01-29 04:37:36 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -9,8 +9,8 @@ using namespace Statistics;
 #include <cmath>
 float arr[4]={1.5, 8.9, 3.2, 4.5};
 TEST_CASE("reports average, minimum and maximum") {
-   // auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
-    auto computedStats = Statistics::ComputeStatistics(arr);
+  auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
+   // auto computedStats = Statistics::ComputeStatistics(arr);
     float epsilon = 0.001;
     REQUIRE(std::abs(computedStats.Mean - 4.525) < epsilon);
     REQUIRE(std::abs(computedStats.max - 8.9) < epsilon);
